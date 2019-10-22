f3029d93239fdf8b5aa2f79f90e2220848f7a8b8
nataliapatrucco <patrucconatalia@gmail.com>
2019-10-22 14:43:05 -0300

    conectado al server

--- models/user.js
+++ models/user.js
@@ -1,28 +1,32 @@
 const S = requiere("sequelize");
+const db = require("./index");
 
 class User extends S.Model {}
-User.init({
-  email: {
-    type: S.STRING,
-    allowNull: false,
-    validate: {
-      isEmail: true
+User.init(
+  {
+    email: {
+      type: S.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: S.STRING,
+      allowNull: false,
+      set() {
+        this.setDataValue("password").this.getDataValue("salt");
+      }
+    },
+    salt: {
+      type: S.STRING,
+      set() {
+        return crypto.randomBytes(20).toString("hex");
+      }
     }
   },
-  password: {
-    type: S.STRING,
-    allowNull: false,
-    set() {
-      this.setDataValue("password").this.getDataValue("salt");
-    }
-  },
-  salt: {
-    type: S.STRING,
-    set() {
-      return crypto.randomBytes(20).toString("hex");
-    }
-  }
-});
+  { sequelize: db, modelName: "user" }
+);
 
 User.prototype.validate = password => {
   crypto
